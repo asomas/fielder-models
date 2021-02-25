15a69a1103a57a122b7452d4f28215713776ca3b
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2021-02-25 15:44:36 +0500

    Added setup file for Python module

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+install_requires = [
+    'djangorestframework>=3.11.0',
+]
+
+setup(
+    name="python_fielder_models",
+    version="0.0.1",
+    description="Django serializer models for Fielder",
+    url="git@github.com:asomas/fielder-models",
+    author="Sarmad Gulzar",
+    author_email="sarmad@asomas.ai",
+    license="MIT",
+    packages=["python_fielder_models"],
+    install_requires=install_requires, 
+    zip_safe=False
+)
