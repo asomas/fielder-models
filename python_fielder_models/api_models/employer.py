6b76b677ebe7646d86b303c7e5fe9a2abdfdf3ef
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2021-03-02 09:17:55 +0500

    Added key contacts serializer

--- python_fielder_models/api_models/employer.py
+++ python_fielder_models/api_models/employer.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 
+from ..db_models.common import PHONE_FIELD_REGEX
+from ..db_models.employer import BillingContact, GeneralContact
+
 
 class EmailExistsRequest(serializers.Serializer):
     email = serializers.EmailField()
@@ -7,3 +10,10 @@ class EmailExistsRequest(serializers.Serializer):
 
 class EmailExistsResponse(serializers.Serializer):
     user_exists = serializers.BooleanField()
+
+
+class KeyContactsRequest(serializers.Serializer):
+    billing_contact = BillingContact(required=True)
+    general_contact = GeneralContact()
+    website = serializers.URLField()
+    phone = serializers.RegexField(PHONE_FIELD_REGEX)
