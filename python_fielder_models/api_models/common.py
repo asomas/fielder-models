7f4dbed6e172ef4bec95998a31001405d1376edf
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2021-03-09 04:42:54 +0500

    Added locations serializers

--- python_fielder_models/api_models/common.py
+++ python_fielder_models/api_models/common.py
@@ -3,3 +3,28 @@ from rest_framework import serializers
 
 class AcceptedResponse(serializers.Serializer):
     accepted = serializers.BooleanField(default=True)
+
+
+class AddressSerializer(serializers.Serializer):
+    building = serializers.CharField(required=False, allow_null=True, default=None)
+    street = serializers.CharField(required=False, allow_null=True, default=None)
+    county = serializers.CharField(required=False, allow_null=True, default=None)
+    city = serializers.CharField(required=False, allow_null=True, default=None)
+    country = serializers.CharField()
+    postal_code = serializers.CharField()
+
+
+class GeolocationSerializer(serializers.Serializer):
+    lat = serializers.FloatField()
+    lng = serializers.FloatField()
+
+
+class GooglePlaceDataSerializer(serializers.Serializer):
+    google_place_id = serializers.CharField(required=True)
+    coords = GeolocationSerializer()
+
+
+class LocationSerializer(serializers.Serializer):
+    address = AddressSerializer()
+    coords = GeolocationSerializer()
+    name = serializers.CharField()
