b40ada592a01d382baa749147055786da34b592c
Hinna Rutaba <rutaba97@gmail.com>
2021-03-16 15:52:39 +0000

    shift status enum added

---
+++ lib/core/enums/slot_status_enums.dart
@@ -0,0 +1,17 @@
+ enum SlotColors{
+  Blue,
+  Green,
+  Orange
+ }
+
+ enum SlotIconStatus{
+  Recurring,
+  Active,
+  Inactive,
+  Missed,
+  ClockedIn,
+  ClockedInLate,
+  ClockedOut,
+  ClockedOutLate,
+  Approved
+ }
