af3df7420235a0d418bbe8623ff1fed2bc07f428
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2021-03-17 14:42:18 +0500

    Made website in general contact nullable

--- python_fielder_models/db_models/employer.py
+++ python_fielder_models/db_models/employer.py
@@ -50,7 +50,7 @@ class GeneralContact(ContactSerializer):
     """Document has fixed ID, general_contact, inside Subcollection called company_info.  So the complete path to this document is  employers/employer_id/company_info/general_contact"""
 
     # note, inherits fields e.g. email from contacts Serializer
-    website = serializers.URLField(required=False)
+    website = serializers.URLField(required=False, allow_null=True)
 
 
 class SICCodeSerializer(serializers.Serializer):
