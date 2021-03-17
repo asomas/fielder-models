4a644c089575366b35e252dd44f81ff5457ad425
Hinna Rutaba <rutaba97@gmail.com>
2021-03-17 20:48:26 +0000

    added location model

--- lib/core/db_models/old/schema/default_location_data_schema.dart
+++ lib/core/db_models/old/schema/default_location_data_schema.dart
@@ -8,9 +8,14 @@ class DefaultLocationDataSchema {
   static const String country = 'country';
   static const String postalCode = 'postal_code';
   static const String coordinates = 'coords';
+  static const String iconUrl = 'icon_url';
   static const String lat = 'lat';
   static const String lng = 'lng';
   static const String cordinates = 'coords';
   static const String formatted_address = 'formatted_address';
   static const String name = 'name';
+  static const String shortName = 'short_name';
+  static const String archived = 'archived';
+  static const String isLive = 'is_live';
+  static const String employerRef = 'employer_ref';
 }
