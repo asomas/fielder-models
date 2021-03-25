6035bd1deac9d8323b3ec42df1a012fb1dc7df97
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2021-03-25 15:30:59 +0500

    CU#fuzx13 Renamed employer to organisation

--- python_fielder_models/api_models/employer.py
+++ python_fielder_models/api_models/employer.py
@@ -5,7 +5,7 @@ from ..db_models.common import (
     HEX_COLOR_REGEX,
     PHONE_FIELD_REGEX,
 )
-from ..db_models.employer import BillingContact, GeneralContact
+from ..db_models.organisation import BillingContact, GeneralContact
 
 
 class EmailExistsRequest(serializers.Serializer):
@@ -17,18 +17,18 @@ class EmailExistsResponse(serializers.Serializer):
 
 
 class KeyContactsRequest(serializers.Serializer):
-    employer_id = serializers.CharField()
+    organisation_id = serializers.CharField()
     billing_contact = BillingContact(required=False)
     general_contact = GeneralContact(required=False)
 
 
 class BrandingRequest(serializers.Serializer):
-    employer_id = serializers.CharField()
+    organisation_id = serializers.CharField()
     brand_color = serializers.RegexField(HEX_COLOR_REGEX)
 
 
 class InviteUsersRequest(serializers.Serializer):
-    employer_id = serializers.CharField()
+    organisation_id = serializers.CharField()
     name = serializers.CharField(max_length=75)
     email = serializers.EmailField()
     role = serializers.ChoiceField(
@@ -37,26 +37,26 @@ class InviteUsersRequest(serializers.Serializer):
     manager = serializers.CharField(required=False, allow_null=True)
 
 
-class OrganizationAcceptInvite(serializers.Serializer):
-    employer_id = serializers.CharField()
+class OrganisationAcceptInvite(serializers.Serializer):
+    organisation_id = serializers.CharField()
     status = serializers.ChoiceField(
         required=True, choices=["accepted", "declined", "pending"]
     )
 
 
 class CompanyCreateRequest(serializers.Serializer):
-    employer_id = serializers.CharField()
+    organisation_id = serializers.CharField()
     company_number = serializers.CharField(max_length=8, min_length=8)
 
 
 class CompanyUpdateVATInfoRequest(serializers.Serializer):
-    employer_id = serializers.CharField()
+    organisation_id = serializers.CharField()
     vat_number = serializers.CharField(max_length=15)
     date_of_registration = serializers.DateField()
 
 
 class UsersListRequest(serializers.Serializer):
-    employer_id = serializers.CharField()
+    organisation_id = serializers.CharField()
 
 
 class UserResponse(serializers.Serializer):
