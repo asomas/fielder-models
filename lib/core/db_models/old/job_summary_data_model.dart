fe8d3485b9d4409bd11237269490827bd7cc5a99
deepakaccount <deepakdogra171@gmail.com>
2021-04-02 16:32:11 +0530

    remname the key

--- lib/core/db_models/old/job_summary_data_model.dart
+++ lib/core/db_models/old/job_summary_data_model.dart
@@ -5,39 +5,33 @@ import 'package:fielder_models/core/db_models/old/schema/job_summary_schema.dart
 import 'package:fielder_models/core/db_models/old/workers_model.dart';
 
 class JobSummaryDataModel {
-  String docID;
-  String organisationID;
+  String organisationId;
   JobDataModel jobDataModel;
-  String jobID;
+  String jobId;
   List<WorkerModel> workersArray;
 
   JobSummaryDataModel({
-    this.docID,
-    this.organisationID,
+    this.organisationId,
     this.jobDataModel,
-    this.jobID,
+    this.jobId,
     this.workersArray,
   }) : assert(
-          docID != null && jobDataModel != null && jobID != null,
+          jobDataModel != null && jobId != null,
         );
 
   factory JobSummaryDataModel.fromMap({
     @required Map<String, dynamic> map,
     @required String docID,
   }) {
-    print("data map is ${map}");
-    print("data doc id is ${docID}");
     if (map.isNotEmpty) {
       try {
         String _organisationID = '';
         final DocumentReference _organisationRef =
             map[JobSummarySchema.organisationRef];
-        print("organisation ref ${_organisationRef ?? null}");
         if (_organisationRef != null) {
           _organisationID = _organisationRef.id;
           String _jobID = '';
           final DocumentReference _jobRef = map[JobSummarySchema.jobRef];
-          print("Job ref ${_jobRef ?? null}");
 
           _jobID = docID;
 
@@ -59,10 +53,9 @@ class JobSummaryDataModel {
 
           if (_jobID.isNotEmpty) {
             return JobSummaryDataModel(
-              docID: docID,
-              organisationID: _organisationID,
+              organisationId: _organisationID,
               jobDataModel: _jobDataModel,
-              jobID: _jobID,
+              jobId: _jobID,
               workersArray: _allWorkerArray,
             );
           }
