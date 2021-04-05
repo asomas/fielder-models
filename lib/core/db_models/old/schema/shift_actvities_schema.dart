c05d419947cd38adab113eb71858888cc6af9c66
Hinna Rutaba <rutaba97@gmail.com>
2021-04-05 10:19:38 +0100

    added shift_activities_data field in shift activities schema

--- lib/core/db_models/old/schema/shift_actvities_schema.dart
+++ lib/core/db_models/old/schema/shift_actvities_schema.dart
@@ -6,5 +6,6 @@ class ShiftActivitiesSchema{
   static const String clockOutTime = "clock_out_time";
   static const String clockOutLocation = "clock_out_location";
   static const String shiftDate = "shift_date";
+  static const String shiftPatternData = "shift_pattern_data";
 
 }
