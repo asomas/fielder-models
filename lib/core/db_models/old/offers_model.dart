a876a812922e4474bd1ab6ce2769967c35bfe1dc
Hinna Rutaba <rutaba97@gmail.com>
2021-05-04 14:05:45 +0500

    changed offers model

--- lib/core/db_models/old/offers_model.dart
+++ lib/core/db_models/old/offers_model.dart
@@ -1,10 +1,13 @@
+import 'package:fielder_models/core/db_models/old/job_data_model.dart';
+import 'package:fielder_models/core/db_models/old/job_summary_data_model.dart';
 import 'package:fielder_models/core/db_models/old/network_models/responses/worker_search_response.dart';
 import 'package:fielder_models/core/db_models/old/pattern_data_model.dart';
+import 'package:fielder_models/core/db_models/old/shift_pattern_data_model.dart';
 import 'package:fielder_models/core/db_models/old/workers_model.dart';
 
 class Offers{
   
-  PatternDataModel shiftPatternData;
+  ShiftPatternDataModel shiftPatternData;
   WorkerModel workerData;
   String status;
   String offerID;
@@ -13,8 +16,8 @@ class Offers{
   
   factory Offers.fromMap(String id, Map<String, dynamic> map)=>
     Offers(
-      shiftPatternData: PatternDataModel.fromMap(map["shift_pattern_id"], map),
-      workerData: WorkerModel.fromMap(map: map["worker_data"], docID: map["worker_ref"].id),
+      shiftPatternData: ShiftPatternDataModel.fromMap(map: map["shift_pattern_data"], docID: map["shift_pattern_ref"]?.id),
+      workerData: WorkerModel.fromMap(map: map["worker_data"], docID: map["worker_ref"]?.id),
       status: map["status"],
       offerID : id,
     );
