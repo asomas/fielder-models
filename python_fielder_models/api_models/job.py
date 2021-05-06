39ecc4f8a7b0c692f06ad513b9af0c5e1c535ef0
Feras Almanna <firas.almanna3@gmail.com>
2021-05-06 14:58:58 +0300

    add approve and unassign api models

---
+++ python_fielder_models/api_models/job.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+class ApproveShiftRequestSerializer(serializers.Serializer):
+    shift_date = serializers.DateField(format="%Y-%m-%d", input_formats=["%Y-%m-%d"], required=False, allow_null=True)
+
+class UnAssignWorkerfromShiftPatternRequestSerializer(serializers.Serializer):
+    worker_id = serializers.CharField() 
+    shift_date = serializers.DateField(format="%Y-%m-%d", input_formats=["%Y-%m-%d"])
+    range = serializers.ChoiceField(
+        (
+        "current",
+        "future",
+        "all"
+        )
+    )
