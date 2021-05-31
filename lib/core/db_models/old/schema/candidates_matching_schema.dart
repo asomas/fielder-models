b041ec715a7908cad47acd824dddbbf763d65688
Hinna Rutaba <rutaba97@gmail.com>
2021-05-31 20:42:24 +0500

    added candidates matching request schema

--- lib/core/db_models/old/schema/candidates_matching_schema.dart
+++ lib/core/db_models/old/schema/candidates_matching_schema.dart
@@ -11,3 +11,19 @@ class CandidatesMatchingSchema {
   static const String availabilityScore = "availability_score";
   static const String overallScore = "overall_score";
 }
+
+class CandidatesMatchingRequestSchema {
+  static const String organisationId = "organisation_id";
+  static const String workerId = "worker_id";
+  static const String isStaff = "is_staff";
+  static const String startDate = "start_date";
+  static const String endDate = "end_date";
+  static const String startTime = "start_time";
+  static const String endTime = "end_time";
+  static const String recurrence = "recurrence";
+  static const String skills = "skills";
+  static const String qualifications = "qualifications";
+  static const String checks = "checks";
+  static const String skip = "skip";
+  static const String limit = "limit";
+}
