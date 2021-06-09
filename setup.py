55af5aff224b336b34a5896f17c182fe5bff8ed4
Feras Almanna <firas.almanna3@gmail.com>
2021-06-09 13:23:13 +0300

    pump backend utils

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ install_requires = [
     "black==20.8b1",
     "djangorestframework>=3.11.0",
     "wheel==0.36.2",
-    "fielder-backend-utils @ git+https://github.com/asomas/fielder-backend-utils.git@v1.0.12#egg=fielder-backend-utils",
+    "fielder-backend-utils @ git+https://github.com/asomas/fielder-backend-utils.git@v1.0.13#egg=fielder-backend-utils",
 ]
 
 setup(
     name="python_fielder_models",
-    version="0.0.115",
+    version="0.0.116",
     description="Django serializer models for Fielder",
     url="git@github.com:asomas/fielder-models",
     author="Sarmad Gulzar",
