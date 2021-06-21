117d3b9419932f0dafd8119840b38cdbccbef0a5
Hinna Rutaba <rutaba97@gmail.com>
2021-06-21 19:32:49 +0500

    changed worker referral document name

--- lib/core/db_models/old/schema/signupReferralsSchema.dart
+++ lib/core/db_models/old/schema/signupReferralsSchema.dart
@@ -4,7 +4,7 @@ class SignupReferralsSchema{
   static const String successfulReferrals = "successful_referrals";
   static const String workerReferralLink = "worker_referral_link";
 
-  static const String referralWorker = "referral_worker";
+  static const String referralWorker = "worker_referral";
   static const String message = "message";
 
 }
