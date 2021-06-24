cc95369ce871feef393a40f79f80dba6eb533015
Hinna Rutaba <rutaba97@gmail.com>
2021-06-24 17:40:25 +0500

    added platform types in intercom verify schema

--- lib/core/db_models/old/schema/intercomVerifySchema.dart
+++ lib/core/db_models/old/schema/intercomVerifySchema.dart
@@ -4,4 +4,8 @@ class IntercomVerifySchema{
   static const String platform = "platform";
   static const String userHash = "user_hash";
 
+  static const String ios = "ios";
+  static const String android = "android";
+  static const String web = "web";
+
 }
