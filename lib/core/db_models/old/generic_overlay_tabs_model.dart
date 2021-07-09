d7a4a656e100a5ae886ff37703e90ea0f21c08cb
Hinna Rutaba <rutaba97@gmail.com>
2021-07-10 00:54:10 +0500

    added to json and from json functions

--- lib/core/db_models/old/generic_overlay_tabs_model.dart
+++ lib/core/db_models/old/generic_overlay_tabs_model.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 
+class OverlayTabsString{
+  static const String label = "label";
+  static const String icon = "icon";
+  static const String view = "view";
+  static const String isDisabled = "isDisabled";
+  static const String isNextDisabled = "isNextDisabled";
+  static const String isBackDisabled = "isBackDisabled";
+  static const String key = "key";
+}
+
 class OverlayTabsModel {
   String label;
   String icon;
@@ -18,4 +28,27 @@ class OverlayTabsModel {
       this.isBackDisabled = false,
       @required this.view})
       : assert(label != null && view != null);
+
+  factory OverlayTabsModel.fromJson(Map json){
+    return OverlayTabsModel(
+      label: json[OverlayTabsString.label],
+      isBackDisabled: json[OverlayTabsString.isBackDisabled],
+      isNextDisabled: json[OverlayTabsString.isNextDisabled],
+      isDisabled: json[OverlayTabsString.isDisabled],
+      icon: json[OverlayTabsString.icon],
+      view: json[OverlayTabsString.view] as Widget,
+    );
+  }
+
+  Map<String, dynamic> toJson(){
+    return {
+      OverlayTabsString.label : label,
+      OverlayTabsString.icon : icon,
+      OverlayTabsString.view : view,
+      OverlayTabsString.isDisabled : isDisabled,
+      OverlayTabsString.isNextDisabled : isNextDisabled,
+      OverlayTabsString.isBackDisabled : isBackDisabled,
+      OverlayTabsString.key : key,
+    };
+  }
 }
