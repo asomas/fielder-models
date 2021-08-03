9cffa704982119d35993ab633bea809cbb50671f
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2021-08-03 14:02:58 +0500

    Bumped the version

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ install_requires = [
     "black==20.8b1",
     "djangorestframework>=3.11.0",
     "wheel==0.36.2",
-    "fielder-backend-utils @ git+https://github.com/asomas/fielder-backend-utils.git@v1.0.18#egg=fielder-backend-utils",
+    "fielder-backend-utils @ git+https://github.com/asomas/fielder-backend-utils.git@v1.0.19#egg=fielder-backend-utils",
 ]
 
 setup(
     name="python_fielder_models",
-    version="0.0.126",
+    version="0.0.127",
     description="Django serializer models for Fielder",
     url="git@github.com:asomas/fielder-models",
     author="Sarmad Gulzar",
