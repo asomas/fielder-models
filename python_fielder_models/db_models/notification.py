9d8b20ed0a0474f3a962fc3afdabe41736e427de
Feras Almanna <firas.almanna3@gmail.com>
2021-08-05 11:42:47 +0300

    add notifications serializers

---
+++ python_fielder_models/db_models/notification.py
@@ -0,0 +1,27 @@
+from fielder_backend_utils.rest_utils import DocumentReferenceField
+from rest_framework import serializers
+from datetime import datetime, timedelta
+
+
+class MiniCardSerializer(serializers.Serializer):
+    title = serializers.CharField()
+    subTitle = serializers.CharField()
+    worker_ref = DocumentReferenceField()
+    read = serializers.BooleanField(default=False)
+    read_at = serializers.DateTimeField(allow_null=True, default=None)
+    created_at = serializers.DateTimeField(default=datetime.now())
+    expires_at = serializers.DateTimeField(default=datetime.now() + timedelta(days=365))
+    dismissed = serializers.BooleanField(default=False)
+    non_dismissible = serializers.BooleanField(default=True)
+    icon = serializers.CharField(allow_null=True, default=None)
+    message_id = serializers.CharField()
+    screen = serializers.CharField(required=False)
+    type = serializers.ChoiceField(choices=["medium_card", "mini_card"])
+
+
+class MediumCardSerializer(MiniCardSerializer):
+    action_button_text = serializers.CharField(required=False)
+    article_url = serializers.URLField(allow_null=True, allow_blank=True)
+    image = serializers.URLField()
+    body = serializers.CharField()
+    expanded = serializers.CharField(default=True)
