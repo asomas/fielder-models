ad24c3d56814b7c5c8d7fc2448416d8700195f2b
Hinna Rutaba <rutaba97@gmail.com>
2021-09-18 16:35:25 +0500

    added right to work model

---
+++ lib/core/db_models/old/right_to_work.dart
@@ -0,0 +1,30 @@
+import 'package:fielder_models/core/db_models/helpers/enum_helpers.dart';
+import 'package:fielder_models/core/db_models/old/schema/right_to_work_schema.dart';
+import 'package:fielder_models/core/enums/enums.dart';
+
+class RightToWork {
+  RightToWorkFlow verificationFlow;
+  RightToWorkVerificationStatus verificationStatus;
+  bool submitted;
+
+  RightToWork({this.verificationFlow, this.verificationStatus, this.submitted});
+
+  factory RightToWork.fromMap(Map map) {
+    try {
+      return RightToWork(
+        verificationFlow: map.containsKey(RightToWorkSchema.verificationPath)
+            ? EnumHelpers.rightToWorkFlowFromString(
+                map[RightToWorkSchema.verificationPath])
+            : RightToWorkFlow.None,
+        verificationStatus: map.containsKey(RightToWorkSchema.status)
+            ? EnumHelpers.rightToWorkVerificationStatusFromString(
+                map[RightToWorkSchema.status])
+            : RightToWorkVerificationStatus.None,
+        submitted: map[RightToWorkSchema.submitted] ?? false,
+      );
+    } catch (e, s) {
+      print('right to work model catch $e, $s');
+      return null;
+    }
+  }
+}
---
+++ lib/core/db_models/old/schema/right_to_work_schema.dart
@@ -0,0 +1,5 @@
+class RightToWorkSchema {
+  static const String verificationPath = 'verification_path';
+  static const String status = 'status';
+  static const String submitted = 'submitted';
+}
