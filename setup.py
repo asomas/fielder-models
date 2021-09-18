7b06174941c83e0d91814cca773c8db2702fe063
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2021-09-18 15:11:46 +0500

    Bumped the version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ install_requires = [
 
 setup(
     name="python_fielder_models",
-    version="0.0.162",
+    version="0.0.163",
     description="Django serializer models for Fielder",
     url="git@github.com:asomas/fielder-models",
     author="Sarmad Gulzar",
