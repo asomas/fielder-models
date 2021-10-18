868553f1396534a8d2eeb02ec60b898dc7562a58
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2021-10-18 17:35:54 +0500

    Added `is_valid` flag in `worker_documents`

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ install_requires = [
 
 setup(
     name="python_fielder_models",
-    version="0.0.181",
+    version="0.0.182",
     description="Django serializer models for Fielder",
     url="git@github.com:asomas/fielder-models",
     author="Sarmad Gulzar",
