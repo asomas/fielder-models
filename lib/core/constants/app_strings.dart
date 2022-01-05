f43c40e8ea9aa9e88b33416e64a437ca8872e8c4
rutaba1 <71274568+rutaba1@users.noreply.github.com>
2022-01-05 23:05:46 +0500

    added enum and helpers for repeat intervals

--- lib/core/constants/app_strings.dart
+++ lib/core/constants/app_strings.dart
@@ -145,6 +145,13 @@ class AppStrings {
   static const String workLocationAddress = "Work Location Address";
   static const String years = "years";
   static const String yes = "Yes";
+  static const String monday = "Monday";
+  static const String tuesday = "Tuesday";
+  static const String wednesday = "Wednesday";
+  static const String thursday = "Thursday";
+  static const String friday = "Friday";
+  static const String saturday = "Saturday";
+  static const String sunday = "Sunday";
   static const String requiredMark = "*";
   static const String version = "Version";
   static const String iAgree = "I agree to following terms and conditions.";
