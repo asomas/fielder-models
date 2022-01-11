afe575ae6d423579305e48c5d02e4faef7e6239f
rutaba1 <71274568+rutaba1@users.noreply.github.com>
2022-01-11 22:13:12 +0500

    added breaks model and updated job, shift pattern and shift activity model

---
+++ lib/core/db_models/old/breaks_model.dart
@@ -0,0 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fielder_models/core/db_models/old/schema/break_schema.dart';
+
+class BreakModel {
+  DateTime startTime;
+  DateTime endTime;
+  int breakLength;
+  bool earlyStop;
+
+  BreakModel({this.startTime, this.endTime, this.breakLength, this.earlyStop});
+
+  factory BreakModel.fromMap(Map map) {
+    try {
+      return BreakModel(
+          startTime: (map[BreakSchema.startTime] as Timestamp)?.toDate(),
+          endTime: (map[BreakSchema.endTime] as Timestamp)?.toDate(),
+          breakLength: map[BreakSchema.breakLength],
+          earlyStop: map[BreakSchema.earlyStop] ?? false);
+    } catch (e, s) {
+      print('break model catch_____${e}______$s');
+      return null;
+    }
+  }
+}
---
+++ lib/core/db_models/old/schema/break_schema.dart
@@ -0,0 +1,6 @@
+class BreakSchema {
+  static const String startTime = 'start_time';
+  static const String endTime = 'end_time';
+  static const String breakLength = 'break_length';
+  static const String earlyStop = 'early_stop';
+}
