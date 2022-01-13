fd41ff508819cc192ce4ef52e71085eb5a1ced15
rutaba1 <71274568+rutaba1@users.noreply.github.com>
2022-01-13 23:12:48 +0500

    removed extra fields from job model and added in budget model

--- lib/core/db_models/old/add_job_model.dart
+++ lib/core/db_models/old/add_job_model.dart
@@ -2,14 +2,10 @@ import 'package:fielder_models/core/db_models/old/additional_info_model.dart';
 import 'package:fielder_models/core/db_models/old/checks_model.dart';
 import 'package:fielder_models/core/db_models/old/courses_and_level_model.dart';
 import 'package:fielder_models/core/db_models/old/job_template_model.dart';
-import 'package:fielder_models/core/db_models/old/pattern_data_model.dart';
 import 'package:fielder_models/core/db_models/old/schema/job_summary_schema.dart';
 import 'package:fielder_models/core/db_models/old/schema/job_template_schema.dart';
-import 'package:fielder_models/core/db_models/old/schema/payment_model_schema.dart';
 import 'package:fielder_models/core/db_models/old/skills_model.dart';
 import 'package:fielder_models/core/db_models/worker/occupation.dart';
-
-import 'default_location_data_model.dart';
 
 class AddJobModel {
   String description;
@@ -23,22 +19,9 @@ class AddJobModel {
   List<dynamic> checks;
   bool isDBSRequired;
   bool isEnhancedDBSRequired;
-  String workLocationAddress;
-  String payCalculation;
-  int lateArrival;
-  int earlyLeaver;
-  int overTimeRate;
-  DefaultLocationDataModel defaultLocationData;
-  List<PatternDataModel> shiftPatternsArray;
-  bool volunteer;
   OccupationModel occupationModel;
-  PaymentModel paymentModel;
-  bool enableEarlyDeduction;
-  bool enableLateDeduction;
-  int overTimeThreshHold;
   bool isArchived;
   List<CoursesAndLevelModel> courses;
-  bool enableUnpaidBreaks;
 
   AddJobModel({
     this.description = '',
@@ -47,52 +30,24 @@ class AddJobModel {
     this.jobReferenceId = '',
     this.selTemplate,
     this.additionalReqsArray,
-    this.workLocationAddress,
-    this.payCalculation = "Shift hours",
-    this.lateArrival = 0,
-    this.earlyLeaver = 0,
-    this.overTimeRate,
     this.isDBSRequired = false,
     this.isEnhancedDBSRequired = false,
     this.skillsArray,
-    this.defaultLocationData,
-    this.shiftPatternsArray,
-    this.volunteer = false,
-    this.enableLateDeduction = false,
-    this.enableEarlyDeduction = false,
     this.checksArray,
     this.checks,
     this.occupationModel,
-    this.paymentModel,
-    this.overTimeThreshHold,
     this.courses,
     this.isArchived = false,
-    this.enableUnpaidBreaks = false,
   });
 
   Map<String, dynamic> toJSON() {
     print('AddJobModel toJSON invoked');
     Map<String, dynamic> _map = {};
     try {
-      print('job created invoked');
-
-      List<dynamic> _shiftPatternsMapArray =
-          shiftPatternsArray?.isNotEmpty == true
-              ? shiftPatternsArray.map((e) => e.toJSON()).toList()
-              : [];
-
       _map = {
         JobTemplateSchema.name: templateName,
         JobTemplateSchema.description: description,
         JobTemplateSchema.jobTitle: title,
-        JobTemplateSchema.volunteer: volunteer ?? false,
-        JobTemplateSchema.payment:
-            PaymentModel(totalCost: paymentModel?.totalCost)
-                .paymentMapForCreateJob(),
-        JobTemplateSchema.payCalculation: payCalculation,
-        JobTemplateSchema.lateArrival: lateArrival,
-        JobTemplateSchema.earlyLeaver: earlyLeaver,
-        JobTemplateSchema.overtimeRate: overTimeRate,
         JobTemplateSchema.occupation: occupationModel.toJson(),
         JobTemplateSchema.skillsIds: (skillsArray?.isNotEmpty == true)
             ? skillsArray.map((e) => e.docID).toList() ?? []
@@ -101,28 +56,14 @@ class AddJobModel {
             (additionalReqsArray?.isNotEmpty == true)
                 ? additionalReqsArray.map((e) => e.docID).toList() ?? []
                 : [],
-        // JobTemplateSchema.defaultLocationdata:
-        //     defaultLocationData.toJSON() ?? {},
-        JobTemplateSchema.shiftPatternData: _shiftPatternsMapArray,
         JobTemplateSchema.checksIds: (checksArray?.isNotEmpty == true)
             ? checksArray.map((e) => e.checkID).toList() ?? []
             : [],
-        JobTemplateSchema.enableEarlyDeduction: enableEarlyDeduction,
-        JobTemplateSchema.enableLateDeduction: enableLateDeduction,
-        JobSummarySchema.overtimeThreshold: overTimeThreshHold,
         JobSummarySchema.isArchived: isArchived ?? false,
         JobSummarySchema.courses: (courses?.isNotEmpty == true)
             ? courses.map((e) => e.toJsonForApi()).toList() ?? []
             : [],
-        JobSummarySchema.enableUnpaidBreaks: enableUnpaidBreaks,
       };
-      // if (volunteer) {
-      //   _map.remove(JobTemplateSchema.payment);
-      // }
-      if (payCalculation == "Actual hours") {
-        _map.remove(JobTemplateSchema.enableLateDeduction);
-        _map.remove(JobTemplateSchema.enableEarlyDeduction);
-      }
       print("AddJobModel map -> $_map");
     } catch (e) {
       print('AddJobModel toJSON error: $e');
@@ -141,22 +82,9 @@ class AddJobModel {
         jobReferenceId: data[JobTemplateSchema.jobReferenceId],
         description: data[JobTemplateSchema.description] ?? "",
         templateName: data[JobTemplateSchema.name] ?? "",
-        volunteer: data[JobTemplateSchema.volunteer] ?? false,
-        payCalculation: data[JobTemplateSchema.payCalculation] ?? "",
-        lateArrival: data[JobTemplateSchema.lateArrival] ?? 0,
-        earlyLeaver: data[JobTemplateSchema.earlyLeaver] ?? 0,
-        overTimeRate: data[JobTemplateSchema.overtimeRate] ?? 0,
         isArchived: data[JobSummarySchema.isArchived] ?? false,
-        enableEarlyDeduction:
-            data[JobTemplateSchema.enableEarlyDeduction] ?? false,
-        enableLateDeduction:
-            data[JobTemplateSchema.enableLateDeduction] ?? false,
-        overTimeThreshHold: data[JobSummarySchema.overtimeThreshold],
         occupationModel: data[JobTemplateSchema.occupation] != null
             ? OccupationModel.fromJson(data[JobTemplateSchema.occupation])
-            : null,
-        paymentModel: data[JobTemplateSchema.payment] != null
-            ? PaymentModel.fromMap(data[JobTemplateSchema.payment])
             : null,
         checksArray:
             (data[JobTemplateSchema.checks] as List)?.isNotEmpty == true
@@ -190,7 +118,6 @@ class AddJobModel {
                 .map((e) => CoursesAndLevelModel.fromMap(e))
                 .toList()
             : [],
-        enableUnpaidBreaks: data[JobSummarySchema.enableUnpaidBreaks],
       );
     }
     return addJobModel;
@@ -202,148 +129,11 @@ class AddJobModel {
     title = '';
     selTemplate = null;
     additionalReqsArray = [];
-    workLocationAddress = '';
     isDBSRequired = false;
     isEnhancedDBSRequired = false;
     skillsArray = [];
     checks = [];
-    defaultLocationData = null;
-    shiftPatternsArray = [];
     checksArray = [];
     occupationModel = null;
-    paymentModel = null;
-    enableUnpaidBreaks = false;
-  }
-
-// sprint 8 work
-
-//  factory AddJobModel.fromJSON(Map<String, dynamic> json) {
-//    List<AdditionalInfoModel> additionalInfoModelList = [];
-//    List<SkillsModel> skillsModelList = [];
-//    List<QualificationModel> qualificationsModelList = [];
-////    DefaultLocationDataModel defaultLocationDataModel = DefaultLocationDataModel(
-////      address: AddressModel(
-////          building: json['location']['address']
-////      ),
-////      coordinates: CoordinatesModel(),
-////    );
-//
-//    final List<dynamic> skills = json[JobTemplateSchema.skills] ?? [];
-//    skills.forEach((element) {
-//      final DocumentReference ref = element['skill_ref'];
-//      final Map<String, dynamic> map = element['skill_value'];
-//      if (ref != null) {
-//        final SkillsModel _skillsModel = SkillsModel.fromMap(
-//            map: map, docID: ref.id);
-//        if (_skillsModel != null) {
-//          skillsModelList.add(_skillsModel);
-//        }
-//      }
-//    });
-
-//
-//    final List<dynamic> qualifications = json[JobTemplateSchema.qualifications];
-//    qualifications.forEach((element) {
-//      final Map<String, dynamic> map = element['qualification_value'];
-//      final DocumentReference ref = element['qualification_ref'];
-//      if (ref != null) {
-//        final QualificationModel _qualificationModel = QualificationModel
-//            .fromMap(map: map, docID: ref.id);
-//        if (_qualificationModel != null) {
-//          qualificationsModelList.add(_qualificationModel);
-//        }
-//      }
-//    });
-//
-//    final List<dynamic> additionalReq =
-//        json[JobTemplateSchema.additionalRequirements] ?? [];
-//    additionalReq.forEach((element) {
-//      final Map<String, dynamic> map = element['additional_requirement_value'];
-//      final DocumentReference ref = element['additional_requirement_ref'];
-//      if (ref != null) {
-//        AdditionalInfoModel _additionalInfoModel = AdditionalInfoModel.fromMap(
-//          map: map,
-//          docID: ref.id,
-//        );
-//        if (_additionalInfoModel != null) {
-//          additionalInfoModelList.add(_additionalInfoModel);
-//        }
-//      }
-//    });
-//    return AddJobModel(
-//        title: json[JobTemplateSchema.jobTitle],
-//        description: json[JobTemplateSchema.description],
-//        rate: json[JobTemplateSchema.rate],
-////      isDBSRequired: json[JobTemplateSchema.]
-//        volunteer: json[JobTemplateSchema.volunteer],
-//        additionalReqsArray: additionalInfoModelList,
-//        skillsArray: skillsModelList,
-//        requiredQualification: qualificationsModelList,
-//        defaultLocationData
-//        :
-//    );
-//  }
-}
-
-class PaymentModel {
-  double fielderMargin;
-  double fielderDiscount;
-  double totalCost;
-  double discountCost;
-  double workerRate;
-  double statuaryCost;
-  double holidayPay;
-
-  static const double onePence = 100.0;
-
-  PaymentModel(
-      {this.fielderMargin,
-      this.fielderDiscount,
-      this.totalCost,
-      this.discountCost,
-      this.workerRate,
-      this.statuaryCost,
-      this.holidayPay});
-
-  factory PaymentModel.fromMap(Map map) {
-    try {
-      double _fielderMargin = (map[PaymentModelSchema.umbrellaFee] +
-              map[PaymentModelSchema.findersFee]) /
-          onePence;
-      double _fielderDiscount = map[PaymentModelSchema.findersFee] / onePence;
-      double _totalCost =
-          map[PaymentModelSchema.totalStaffingServiceCost] / onePence;
-      double _discountCost =
-          map[PaymentModelSchema.totalUmbrellaServiceCost] / onePence;
-      double _workerRate = map[PaymentModelSchema.workerRate] / onePence;
-      double _holidayPay = map[PaymentModelSchema.holidayPay] / onePence;
-      double _statuaryCost = 0;
-      if (map[PaymentModelSchema.statutaryCosts] != null) {
-        _statuaryCost = map[PaymentModelSchema.statutaryCosts]
-                [PaymentModelSchema.total] /
-            onePence;
-      }
-      return PaymentModel(
-          workerRate: _workerRate,
-          fielderDiscount: _fielderDiscount,
-          fielderMargin: _fielderMargin,
-          totalCost: _totalCost,
-          discountCost: _discountCost,
-          holidayPay: _holidayPay,
-          statuaryCost: _statuaryCost);
-    } catch (e, s) {
-      print("payment model catch_____${e}_____$s");
-      return null;
-    }
-  }
-
-  Map paymentMapForCreateJob() {
-    if (totalCost != null) {
-      return {
-        PaymentModelSchema.totalStaffingServiceCost:
-            (totalCost * onePence).round()
-      };
-    }
-    return {};
   }
 }
---
+++ lib/core/db_models/old/budget_model.dart
@@ -0,0 +1,152 @@
+import 'package:fielder_models/core/db_models/helpers/enum_helpers.dart';
+import 'package:fielder_models/core/db_models/old/schema/job_summary_schema.dart';
+import 'package:fielder_models/core/db_models/old/schema/job_template_schema.dart';
+import 'package:fielder_models/core/db_models/old/schema/payment_model_schema.dart';
+import 'package:fielder_models/core/enums/enums.dart';
+
+class BudgetModel {
+  CalculatePay payCalculation;
+  int lateArrival;
+  int earlyLeaver;
+  int overTimeRate;
+  bool volunteer;
+  PaymentModel paymentModel;
+  bool enableEarlyDeduction;
+  bool enableLateDeduction;
+  int overTimeThreshHold;
+  bool enableUnpaidBreaks;
+
+  BudgetModel({
+    this.payCalculation = CalculatePay.ShiftHours,
+    this.lateArrival = 0,
+    this.earlyLeaver = 0,
+    this.overTimeRate,
+    this.volunteer = false,
+    this.enableLateDeduction = false,
+    this.enableEarlyDeduction = false,
+    this.paymentModel,
+    this.overTimeThreshHold,
+    this.enableUnpaidBreaks = false,
+  });
+
+  factory BudgetModel.fromMap(Map data) {
+    try {
+      return BudgetModel(
+        volunteer: data[JobTemplateSchema.volunteer] ?? false,
+        payCalculation: data[JobTemplateSchema.payCalculation] != null
+            ? EnumHelpers.getPayTypeFromString(
+                data[JobTemplateSchema.payCalculation])
+            : null,
+        lateArrival: data[JobTemplateSchema.lateArrival] ?? 0,
+        earlyLeaver: data[JobTemplateSchema.earlyLeaver] ?? 0,
+        overTimeRate: data[JobTemplateSchema.overtimeRate] ?? 0,
+        enableEarlyDeduction:
+            data[JobTemplateSchema.enableEarlyDeduction] ?? false,
+        enableLateDeduction:
+            data[JobTemplateSchema.enableLateDeduction] ?? false,
+        overTimeThreshHold: data[JobSummarySchema.overtimeThreshold],
+        paymentModel: data[JobTemplateSchema.payment] != null
+            ? PaymentModel.fromMap(data[JobTemplateSchema.payment])
+            : null,
+        enableUnpaidBreaks: data[JobSummarySchema.enableUnpaidBreaks],
+      );
+    } catch (e, s) {
+      print("Budget Model catch_____${e}____$s");
+      return null;
+    }
+  }
+
+  Map<String, dynamic> toJSON() {
+    Map<String, dynamic> _map = {};
+    try {
+      _map = {
+        JobTemplateSchema.volunteer: volunteer ?? false,
+        JobTemplateSchema.payment:
+            PaymentModel(totalCost: paymentModel?.totalCost)
+                .paymentMapForCreateJob(),
+        JobTemplateSchema.payCalculation: payCalculation,
+        JobTemplateSchema.lateArrival: lateArrival,
+        JobTemplateSchema.earlyLeaver: earlyLeaver,
+        JobTemplateSchema.overtimeRate: overTimeRate,
+        JobTemplateSchema.enableEarlyDeduction: enableEarlyDeduction,
+        JobTemplateSchema.enableLateDeduction: enableLateDeduction,
+        JobSummarySchema.overtimeThreshold: overTimeThreshHold,
+        JobSummarySchema.enableUnpaidBreaks: enableUnpaidBreaks,
+      };
+      // if (volunteer) {
+      //   _map.remove(JobTemplateSchema.payment);
+      // }
+      if (payCalculation == CalculatePay.ClockedInHours) {
+        _map.remove(JobTemplateSchema.enableLateDeduction);
+        _map.remove(JobTemplateSchema.enableEarlyDeduction);
+      }
+      print("Budget Model map -> $_map");
+    } catch (e) {
+      print('Budget Model toJSON error: $e');
+    }
+    return _map;
+  }
+}
+
+class PaymentModel {
+  double fielderMargin;
+  double fielderDiscount;
+  double totalCost;
+  double discountCost;
+  double workerRate;
+  double statuaryCost;
+  double holidayPay;
+
+  static const double onePence = 100.0;
+
+  PaymentModel(
+      {this.fielderMargin,
+      this.fielderDiscount,
+      this.totalCost,
+      this.discountCost,
+      this.workerRate,
+      this.statuaryCost,
+      this.holidayPay});
+
+  factory PaymentModel.fromMap(Map map) {
+    try {
+      double _fielderMargin = (map[PaymentModelSchema.umbrellaFee] +
+              map[PaymentModelSchema.findersFee]) /
+          onePence;
+      double _fielderDiscount = map[PaymentModelSchema.findersFee] / onePence;
+      double _totalCost =
+          map[PaymentModelSchema.totalStaffingServiceCost] / onePence;
+      double _discountCost =
+          map[PaymentModelSchema.totalUmbrellaServiceCost] / onePence;
+      double _workerRate = map[PaymentModelSchema.workerRate] / onePence;
+      double _holidayPay = map[PaymentModelSchema.holidayPay] / onePence;
+      double _statuaryCost = 0;
+      if (map[PaymentModelSchema.statutaryCosts] != null) {
+        _statuaryCost = map[PaymentModelSchema.statutaryCosts]
+                [PaymentModelSchema.total] /
+            onePence;
+      }
+      return PaymentModel(
+          workerRate: _workerRate,
+          fielderDiscount: _fielderDiscount,
+          fielderMargin: _fielderMargin,
+          totalCost: _totalCost,
+          discountCost: _discountCost,
+          holidayPay: _holidayPay,
+          statuaryCost: _statuaryCost);
+    } catch (e, s) {
+      print("payment model catch_____${e}_____$s");
+      return null;
+    }
+  }
+
+  Map paymentMapForCreateJob() {
+    if (totalCost != null) {
+      return {
+        PaymentModelSchema.totalStaffingServiceCost:
+            (totalCost * onePence).round()
+      };
+    }
+    return {};
+  }
+}
