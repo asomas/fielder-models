bde25748f015d8b3994aa5a5d7c09a63ea785466
rutaba1 <71274568+rutaba1@users.noreply.github.com>
2022-03-23 21:32:45 +0500

    updated videos overlay

--- lib/core/db_models/worker/appInfo/supportVideoOrganisation.dart
+++ lib/core/db_models/worker/appInfo/supportVideoOrganisation.dart
@@ -1,20 +1,29 @@
 import 'package:fielder_models/core/db_models/worker/schema/supportVideoOrganisationSchema.dart';
 
-class SupportVideoOrganisation{
+class SupportVideoOrganisation {
   SupportVideoOrganisation({
     this.jobSummary,
     this.staffSummary,
-    this.notificationsSummary
+    this.notificationsSummary,
+    this.createShift,
+    this.assignShift,
+    this.getHelp,
   });
 
   String jobSummary;
   String staffSummary;
   String notificationsSummary;
+  String createShift;
+  String assignShift;
+  String getHelp;
 
-
-  factory SupportVideoOrganisation.fromJson(Map<String, dynamic> json) => SupportVideoOrganisation(
-    jobSummary: json[SupportVideoOrganisationSchema.jobSummary]??"",
-    staffSummary: json[SupportVideoOrganisationSchema.staffSummary]??"",
-    notificationsSummary: json[SupportVideoOrganisationSchema.notificationsSummary]??"",
-  );
+  factory SupportVideoOrganisation.fromJson(Map<String, dynamic> json) =>
+      SupportVideoOrganisation(
+          jobSummary: json[SupportVideoOrganisationSchema.jobSummary] ?? "",
+          staffSummary: json[SupportVideoOrganisationSchema.staffSummary] ?? "",
+          notificationsSummary:
+              json[SupportVideoOrganisationSchema.notificationsSummary] ?? "",
+          assignShift: json[SupportVideoOrganisationSchema.assignShift] ?? "",
+          createShift: json[SupportVideoOrganisationSchema.createShift] ?? "",
+          getHelp: json[SupportVideoOrganisationSchema.getHelp] ?? "");
 }
