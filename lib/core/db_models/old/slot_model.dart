ede6abb41dcdbefb67fd00544d5ed506af50ea22
rutaba1 <71274568+rutaba1@users.noreply.github.com>
2022-05-31 11:33:34 +0500

    Updating slot model (#150)
    * added color param in slot model
    
    * added enum helper for slot status icon

--- lib/core/db_models/old/slot_model.dart
+++ lib/core/db_models/old/slot_model.dart
@@ -2,11 +2,12 @@ import 'package:fielder_models/core/db_models/old/interview_model.dart';
 import 'package:fielder_models/core/db_models/old/shift_activities_model.dart';
 import 'package:fielder_models/core/db_models/old/shift_pattern_data_model.dart';
 import 'package:fielder_models/core/enums/slot_status_enums.dart';
+import 'package:flutter/material.dart';
 
 class SlotModel {
   final String shiftId;
-  final DateTime startTime;
-  final DateTime endTime;
+  final DateTime startAt;
+  final DateTime endAt;
   final String slotText;
   String workerAvatarUrl;
   final String workerId;
@@ -20,26 +21,33 @@ class SlotModel {
   final bool isHead;
   final bool isTail;
   final InterviewModel interviewModel;
+  final Color slotColor;
   int timeRowIndex;
+  bool allDay;
+  //List<String> resourceIds;
 
-  SlotModel(
-      {this.shiftId,
-      this.slotText = '',
-      this.workerAvatarUrl,
-      this.startTime,
-      this.endTime,
-      this.workerId,
-      this.workerName = '',
-      this.weekDay,
-      this.shiftPatternDataModel,
-      this.shiftActivitiesModel,
-      this.slotStatusIcon = SlotStatusIcon.Inactive,
-      this.isUnavailable = false,
-      this.spanMultipleDays = false,
-      this.isHead = false,
-      this.isTail = false,
-      this.interviewModel,
-      this.timeRowIndex = 1});
+  SlotModel({
+    this.shiftId,
+    this.slotText = '',
+    this.workerAvatarUrl,
+    this.startAt,
+    this.endAt,
+    this.workerId,
+    this.workerName = '',
+    this.weekDay,
+    this.shiftPatternDataModel,
+    this.shiftActivitiesModel,
+    this.slotStatusIcon = SlotStatusIcon.Inactive,
+    this.isUnavailable = false,
+    this.spanMultipleDays = false,
+    this.isHead = false,
+    this.isTail = false,
+    this.interviewModel,
+    this.timeRowIndex = 1,
+    @required this.slotColor,
+    this.allDay = false,
+    //this.resourceIds,
+  });
 
   @override
   bool operator ==(other) {
