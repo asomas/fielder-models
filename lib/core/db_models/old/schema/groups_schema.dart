9e73834f2a63142646e5cbd9cae82a0c5492ba85
rutaba1 <71274568+rutaba1@users.noreply.github.com>
2022-06-03 01:21:24 +0500

    added new params in group model and billing model

--- lib/core/db_models/old/schema/groups_schema.dart
+++ lib/core/db_models/old/schema/groups_schema.dart
@@ -5,4 +5,6 @@ class GroupsSchema {
   static const String groupRole = "group_role";
   static const String organisationRef = "organisation_ref";
   static const String organisationUserRef = "organisation_user_ref";
+  static const String billingEmail = "billing_email";
+  static const String groupBillingEnabled ="group_billing_enabled";
 }
--- lib/core/db_models/old/schema/organisation_user_schema.dart
+++ lib/core/db_models/old/schema/organisation_user_schema.dart
@@ -3,4 +3,7 @@ class OrganisationUserRelationSchema {
   static const String organisationRef = 'organisation_ref';
   static const String organisationUserRef = 'organisation_user_ref';
   static const String status = 'status';
+  static const String canChangeRole = 'can_change_group_role';
+  static const String email = 'email';
+  static const String role = 'role';
 }
