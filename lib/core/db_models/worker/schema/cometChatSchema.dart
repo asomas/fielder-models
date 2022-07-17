913f681946a2b46b5558d75471ab3bb1b4a730f1
Dibash Poudel <31642846+dbspoudel@users.noreply.github.com>
2022-07-17 15:21:20 +0545

    added cometChatSchema.dart (#173)

---
+++ lib/core/db_models/worker/schema/cometChatSchema.dart
@@ -0,0 +1,23 @@
+class CometChatSchema {
+  static const String authToken = "auth_token";
+  static const String lastMessage = "lastMessage";
+  static const String id = "id";
+  static const String type = "type";
+  static const String sender = "sender";
+  static const String metadata = "metadata";
+  static const String unreadMessageCount = "unreadMessageCount";
+  static const String receiverId = "receiverId";
+  static const String uid = "uid";
+  static const String deletedAt = "deletedAt";
+  static const String attachments = "attachments";
+  static const String size = "size";
+  static const String name = "name";
+  static const String url = "url";
+  static const String senderUserId = "sender_user_id";
+  static const String senderName = "sender_name";
+  static const String text = "text";
+  static const String image = "image";
+  static const String file = "file";
+  static const String conversationWith = "conversationWith";
+  static const String data = "data";
+}
