9339fd5d35acc7cfda39a4d0ddb8a570a34bed14
rutaba1 <71274568+rutaba1@users.noreply.github.com>
2022-08-11 14:15:22 +0500

    updated models for skills and carousel (#181)

--- lib/core/db_models/old/media_model.dart
+++ lib/core/db_models/old/media_model.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fielder_models/core/db_models/helpers/helpers.dart';
+import 'package:fielder_models/core/db_models/old/welcome_carousel.dart';
 import 'package:fielder_models/core/enums/enums.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart' as p;
@@ -6,12 +8,15 @@ import 'package:path/path.dart' as p;
 class MediaModel {
   final String path;
   final MediaType type;
+  final DateTime updatedAt;
 
   const MediaModel({
     @required this.path,
     @required this.type,
+    @required this.updatedAt,
   });
-  bool get isValid => path?.isNotEmpty??false;
+
+  bool get isValid => path?.isNotEmpty ?? false;
 
   bool get isLocal => Helpers.isUrl(path);
 
@@ -24,6 +29,21 @@ class MediaModel {
       return MediaModel(
         path: url,
         type: _getUrlType(url),
+        updatedAt: null,
+      );
+    } catch (e, s) {
+      print('media model url catch____${e}____$s');
+      return null;
+    }
+  }
+
+  factory MediaModel.fromMap(Map data) {
+    try {
+      return MediaModel(
+        path: data[WelcomeCarouselSchema.media],
+        type: _getUrlType(data[WelcomeCarouselSchema.media]),
+        updatedAt:
+            (data[WelcomeCarouselSchema.updatedAt] as Timestamp)?.toDate(),
       );
     } catch (e, s) {
       print('media model catch____${e}____$s');
@@ -35,6 +55,7 @@ class MediaModel {
     return MediaModel(
       path: json['path'],
       type: _getUrlType(json['path']),
+      updatedAt: null,
     );
   }
 
@@ -44,10 +65,11 @@ class MediaModel {
     };
   }
 
-  MediaModel copyWith(String path, MediaType type) {
+  MediaModel copyWith(String path, MediaType type, DateTime updatedat) {
     return MediaModel(
       path: path ?? this.path,
       type: type ?? this.type,
+      updatedAt: updatedAt ?? this.updatedAt,
     );
   }
 
---
+++ lib/core/db_models/old/schema/skill_rating_schema.dart
@@ -0,0 +1,10 @@
+class SkillRatingSchema {
+  static const String transferable = 'transferable';
+  static const String specialized = 'specialized';
+  static const String id = 'id';
+  static const String description = 'description';
+  static const String transferability = 'transferability';
+  static const String value = 'value';
+  static const String skillId = 'skill_id';
+  static const String rating = 'rating';
+}
