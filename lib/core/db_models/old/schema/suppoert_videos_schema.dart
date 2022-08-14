08bd6f2b80b1b9fb2545f7071f87cf8aec4910ba
rutaba1 <71274568+rutaba1@users.noreply.github.com>
2022-08-14 18:11:46 +0500

    Create suppoert_videos_schema.dart (#182)

---
+++ lib/core/db_models/old/schema/suppoert_videos_schema.dart
@@ -0,0 +1,8 @@
+class SupportVideosSchema {
+  static const String supportVideosWorker = 'support_videos_worker';
+  static const String availability = 'availability';
+  static const String experience = 'experience';
+  static const String growth = 'growth';
+  static const String home = 'home';
+  static const String shifts = 'shifts';
+}
