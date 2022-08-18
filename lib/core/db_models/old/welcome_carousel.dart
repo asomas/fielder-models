f0018f3666ca72f5f4d733976ee9a9daf28090a8
rutaba1 <71274568+rutaba1@users.noreply.github.com>
2022-08-18 19:34:52 +0500

    updated media model

--- lib/core/db_models/old/welcome_carousel.dart
+++ lib/core/db_models/old/welcome_carousel.dart
@@ -1,16 +1,19 @@
 import 'package:fielder_models/core/db_models/old/media_model.dart';
+import 'package:fielder_models/core/db_models/old/schema/table_collection_schema.dart';
 
 class WelcomeCarouselModel {
   List<MediaModel> data;
 
-  WelcomeCarouselModel({this.data});
+  WelcomeCarouselModel({this.data = const []});
 
   factory WelcomeCarouselModel.fromMap(Map map) {
     try {
       return WelcomeCarouselModel(
-          data: (map[WelcomeCarouselSchema.carouselData] as List)
-              .map((e) => MediaModel.fromMap(e))
-              .toList());
+        data: (map[FbCollections.images] as List)
+                .map((e) => MediaModel.fromUrl(e))
+                .toList() ??
+            [],
+      );
     } catch (e, s) {
       print('welcome carousel catch____${e}____$s');
       return null;
@@ -20,6 +23,6 @@ class WelcomeCarouselModel {
 
 class WelcomeCarouselSchema {
   static const String carouselData = 'carousel_data';
-  static const String media = 'media';
+  static const String url = 'url';
   static const String updatedAt = 'updated_at';
 }
