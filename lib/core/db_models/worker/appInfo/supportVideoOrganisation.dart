d61b7f8ebe950951cca037295b5e0f7853caec93
rutaba1 <71274568+rutaba1@users.noreply.github.com>
2022-08-19 11:53:01 +0500

    added new field for organisation setup

--- lib/core/db_models/worker/appInfo/supportVideoOrganisation.dart
+++ lib/core/db_models/worker/appInfo/supportVideoOrganisation.dart
@@ -8,6 +8,7 @@ class SupportVideoOrganisation {
     this.createShift,
     this.assignShift,
     this.getHelp,
+    this.organisationSetup,
   });
 
   String jobSummary;
@@ -16,14 +17,18 @@ class SupportVideoOrganisation {
   String createShift;
   String assignShift;
   String getHelp;
+  String organisationSetup;
 
   factory SupportVideoOrganisation.fromJson(Map<String, dynamic> json) =>
       SupportVideoOrganisation(
-          jobSummary: json[SupportVideoOrganisationSchema.jobSummary] ?? "",
-          staffSummary: json[SupportVideoOrganisationSchema.staffSummary] ?? "",
-          notificationsSummary:
-              json[SupportVideoOrganisationSchema.notificationsSummary] ?? "",
-          assignShift: json[SupportVideoOrganisationSchema.assignShift] ?? "",
-          createShift: json[SupportVideoOrganisationSchema.createShift] ?? "",
-          getHelp: json[SupportVideoOrganisationSchema.getHelp] ?? "");
+        jobSummary: json[SupportVideoOrganisationSchema.jobSummary] ?? "",
+        staffSummary: json[SupportVideoOrganisationSchema.staffSummary] ?? "",
+        notificationsSummary:
+            json[SupportVideoOrganisationSchema.notificationsSummary] ?? "",
+        assignShift: json[SupportVideoOrganisationSchema.assignShift] ?? "",
+        createShift: json[SupportVideoOrganisationSchema.createShift] ?? "",
+        getHelp: json[SupportVideoOrganisationSchema.getHelp] ?? "",
+        organisationSetup:
+            json[SupportVideoOrganisationSchema.organisation_setup] ?? "",
+      );
 }
--- lib/core/db_models/worker/schema/supportVideoOrganisationSchema.dart
+++ lib/core/db_models/worker/schema/supportVideoOrganisationSchema.dart
@@ -5,4 +5,5 @@ class SupportVideoOrganisationSchema {
   static const String assignShift = "assign_shift";
   static const String createShift = "create_shift";
   static const String getHelp = "get_help";
+  static const String organisation_setup = "organisation_setup";
 }
