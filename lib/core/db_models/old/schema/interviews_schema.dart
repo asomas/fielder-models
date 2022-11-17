bbfc12dce4d5ea3a185cd29fbbf2c653deb68728
Dibash Poudel <31642846+dbspoudel@users.noreply.github.com>
2022-11-18 04:24:09 +0545

    Update interviews_schema.dart (#212)

--- lib/core/db_models/old/schema/interviews_schema.dart
+++ lib/core/db_models/old/schema/interviews_schema.dart
@@ -3,6 +3,7 @@ class InterviewsSchema {
   static const String breakDuration = "break_duration";
   static const String interviewType = "interview_type";
   static const String organisationId = "organisation_id";
+  static const String groupId = "group_id";
   static const String organisationUserId = "organisation_user_id";
   static const String repeatCount = "repeat_count";
   static const String startTime = "start_time";
@@ -10,6 +11,7 @@ class InterviewsSchema {
   static const String interviewSlotId = "interview_slot_id";
   static const String assigned = "assigned";
   static const String organisationRef = "organisation_ref";
+  static const String groupRef = "group_ref";
   static const String organisationUserRef = "organisation_user_ref";
   static const String address = "address";
   static const String interviewAddress = "interview_address";
