44ca0ce560299e4471104326ba1650d5904ab0e4
Dibash Poudel <dbspoudel@gmail.com>
2022-12-08 09:38:54 +0545

    add fields on checkModel

--- lib/core/db_models/old/checks_model.dart
+++ lib/core/db_models/old/checks_model.dart
@@ -1,13 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fielder_models/core/db_models/old/schema/check_model_schema.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:fielder_models/core/db_models/old/schema/job_template_schema.dart';
 
 class CheckModel {
   String checkID;
   String value;
+  String label;
+  List<DocumentReference> dependsOn;
 
   CheckModel({
     this.checkID,
     this.value,
+    this.label,
+    this.dependsOn,
   });
 
   factory CheckModel.fromMap({
@@ -16,15 +22,20 @@ class CheckModel {
   }) {
     if (map.isNotEmpty) {
       String _value;
-      if (map.containsKey("value")) {
-        _value = map['value'] ?? '';
+      if (map.containsKey(CheckModelSchema.value)) {
+        _value = map[CheckModelSchema.value] ?? '';
       } else if (map.containsKey(JobTemplateSchema.checkValue)) {
         _value = map[JobTemplateSchema.checkValue] ?? '';
       }
+      final _label = map[CheckModelSchema.checkInstructionText];
+      final List<DocumentReference> _dependentIds =
+          (map[CheckModelSchema.checkDependsOn] as List).map((e) => e as DocumentReference).toList();
       if (_value.isNotEmpty) {
         return CheckModel(
           checkID: checkID,
           value: _value,
+          label: _label,
+          dependsOn: _dependentIds,
         );
       }
     }
