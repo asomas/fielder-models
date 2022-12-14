acfc2412f70e3a123bc0e73b922f8a425cb70462
feras <firas.almanna3@gmail.com>
2022-12-14 13:53:58 +0300

    Revert "fix skill importance name"
    This reverts commit e97527de23eb1b91bef8a5f33534258726730d07.

--- python_fielder_models/common/taxonomy.py
+++ python_fielder_models/common/taxonomy.py
@@ -6,7 +6,7 @@ class SkillSerializer(serializers.Serializer):
     skill_ref = DocumentReferenceField()
     skill_value = serializers.CharField()
     relevancy_score = serializers.FloatField(required=False)
-    importance = serializers.IntegerField(min_value=0, max_value=1, default=0)
+    important = serializers.IntegerField(min_value=0, max_value=1, default=0)
     category = serializers.CharField(required=False)
 
 
