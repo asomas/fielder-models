1e67480e20bfeabb2a395e41d632129ab345b421
Dibash Poudel <31642846+dbspoudel@users.noreply.github.com>
2022-12-15 14:52:58 +0545

    add unreadMessages notification schema & typeModel (#223)

--- lib/core/db_models/old/notification_setting_model.dart
+++ lib/core/db_models/old/notification_setting_model.dart
@@ -18,6 +18,7 @@ class NotificationSettingModel {
           NotificationTypeModel(),
       NotificationSettingSchema.lateShiftClockin: NotificationTypeModel(),
       NotificationSettingSchema.upcomingInterview: NotificationTypeModel(),
+      NotificationSettingSchema.unreadMessages: NotificationTypeModel(),
     });
   }
 
