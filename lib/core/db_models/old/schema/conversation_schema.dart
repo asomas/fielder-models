c07fa15726d6a429037d7875643ab83c1c7120b7
rutaba1 <71274568+rutaba1@users.noreply.github.com>
2023-01-07 23:49:28 +0500

    added conversation model and schema

---
+++ lib/core/db_models/old/schema/conversation_schema.dart
@@ -0,0 +1,22 @@
+class ConversationSchema {
+  static const String conversationId = 'conversationId';
+  static const String conversationType = 'conversationType';
+  static const String lastMessage = 'lastMessage';
+  static const String conversationWith = 'conversationWith';
+  static const String unreadMessageCount = 'unreadMessageCount';
+  static const String data = 'data';
+  static const String entities = 'entities';
+  static const String text = 'text';
+  static const String sender = 'sender';
+  static const String entity = 'entity';
+  static const String uid = 'uid';
+  static const String name = 'name';
+  static const String role = 'role';
+  static const String status = 'status';
+  static const String lastActiveAt = 'lastActiveAt';
+  static const String entityType = 'entityType';
+  static const String receiver = 'receiver';
+  static const String hasBlockedMe = 'hasBlockedMe';
+  static const String blockedByMe = 'blockedByMe';
+  static const String deactivatedAt = 'deactivatedAt';
+}
