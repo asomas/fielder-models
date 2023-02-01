53340f728f432d3b2d58cd77d9818ca7ba061e1b
srutaba1 <hinna@asomas.ai>
2023-02-01 21:22:20 +0500

    fixed checks enums

--- lib/core/db_models/old/schema/worker_checks_schema.dart
+++ lib/core/db_models/old/schema/worker_checks_schema.dart
@@ -12,4 +12,44 @@ class WorkerChecksSchema {
   static const String expectedCompletionAt = 'expected_completion_at';
   static const String organisationRef = 'organisation_ref';
   static const String workerAppScreen = 'worker_app_screen';
+
+  //--------------On Boarding-------------------------
+  static const String onBoardingValue = 'Let’s get started with Fielder';
+  static const String onBoardingId = 'on_boarding';
+
+  //--------------Approved Gaps-------------------------
+  static const String approvedGapsValue = 'Approved Gaps';
+  static const String approvedGapsId = 'approved_gaps';
+
+  //--------------DBS-------------------------
+  static const String dbsValue = 'DBS';
+  static const String dbsId = 'dbs';
+
+  //--------------Enhanced DBS-------------------------
+  static const String enhancedDbsValue = 'Enhanced DBS';
+  static const String enhancedDbsId = 'enhanced_dbs';
+
+  //--------------All Experiences Referenced-------------------------
+  static const String experiencedReferencedValue = 'All Experiences Referenced';
+  static const String experiencedReferencedId = 'experiences_referenced';
+
+  //--------------Logged in-------------------------
+  static const String loggedInValue = 'Logged in';
+  static const String loggedInId = 'logged_in';
+
+  //--------------Proof of Address-------------------------
+  static const String proofOfAddressValue = 'Proof of Address';
+  static const String proofOfAddressId = 'proof_of_address';
+
+  //--------------Proof of ID-------------------------
+  static const String proofOfIDValue = 'Proof of ID';
+  static const String proofOfIdId = 'proof_of_id';
+
+  //--------------RTWs-------------------------
+  static const String RTWValue = 'RTW';
+  static const String RTWId = 'right_to_work';
+
+  //--------------Five Year Work History-------------------------
+  static const String fiveYearWorkHistoryValue = 'Five Year Work History';
+  static const String fiveYearWorkHistoryId = 'work_history';
 }
